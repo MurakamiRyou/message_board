40f7f2cf1d0daa2b9f2b39945a1a100bc16376d9
ryo-mura <ryo-mura@NA21038.nature.local>
2021-07-19 13:10:15 +0900

    データベース関連のファイルを追加

--- src/models/Message.java
+++ src/models/Message.java
@@ -33,4 +33,40 @@ public class Message {
 		return id;
 	}
 
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getContent() {
+		return content;
+	}
+
+	public void setContent(String content) {
+		this.content = content;
+	}
+
+	public Timestamp getCreated_at() {
+		return created_at;
+	}
+
+	public void setCreated_at(Timestamp created_at) {
+		this.created_at = created_at;
+	}
+
+	public Timestamp getUpdated_at() {
+		return updated_at;
+	}
+
+	public void setUpdated_at(Timestamp updated_at) {
+		this.updated_at = updated_at;
+	}
+
 }
